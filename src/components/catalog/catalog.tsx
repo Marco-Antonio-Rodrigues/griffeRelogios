2d0969fcba8e039d06c3e62f78a8554f95ff3808
Marco Antonio Rodrigues Gomes de Souza <94053824+Marco-Antonio-Rodrigues@users.noreply.github.com>
2023-04-22 22:25:32 -0300

    bug tag src

--- src/components/catalog/catalog.tsx
+++ src/components/catalog/catalog.tsx
@@ -1,11 +1,12 @@
 import './catalog.css'
+import qrcode from '../../assets/qrcode.png'
 
 function Catalog(){
   return(
     <div id='catalog' className='catalog'>
       <p>Acesse nosso catálogo do whatsapp clicando no botão ou escaneando o QR code:</p>
       <div  className='catalog-center'>
-        <img alt='Qr code para catálogo' src='/assets/qrcode.png' className='qrcode'></img>
+        <img alt='Qr code para catálogo' src={qrcode} className='qrcode'></img>
         <a href='https://www.whatsapp.com/catalog/558581711232/?app_absent=0' className='botton-catalog'>
           <span>Catálogo Whatsapp</span>
           <img src='src/assets/logowhatsapp.png' className='logowhatsapp'></img>
