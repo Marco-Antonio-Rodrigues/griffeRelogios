3e34fe4c82256965b7bb21f07d8def53dd47f60f
Marco Antonio Rodrigues Gomes de Souza <94053824+Marco-Antonio-Rodrigues@users.noreply.github.com>
2023-04-22 22:10:39 -0300

    corrigindo endereço de imagem tag src

--- src/components/catalog/catalog.tsx
+++ src/components/catalog/catalog.tsx
@@ -5,7 +5,7 @@ function Catalog(){
     <div id='catalog' className='catalog'>
       <p>Acesse nosso catálogo do whatsapp clicando no botão ou escaneando o QR code:</p>
       <div  className='catalog-center'>
-        <img alt='Qr code para catálogo' src='src/assets/qrcode.png' className='qrcode'></img>
+        <img alt='Qr code para catálogo' src='./src/assets/qrcode.png' className='qrcode'></img>
         <a href='https://www.whatsapp.com/catalog/558581711232/?app_absent=0' className='botton-catalog'>
           <span>Catálogo Whatsapp</span>
           <img src='src/assets/logowhatsapp.png' className='logowhatsapp'></img>
